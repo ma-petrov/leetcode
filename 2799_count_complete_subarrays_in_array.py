bddbda0201ae2309f7ad6620395c3a80a2bb0322
Petrov Mikhail <petrov.ma@icloud.com>
2025-04-24 04:19:49 +0300

    change solution

--- 2799_count_complete_subarrays_in_array.py
+++ 2799_count_complete_subarrays_in_array.py
@@ -1,46 +1,41 @@
 # https://leetcode.com/problems/count-complete-subarrays-in-an-array/
 
 
+import collections
+
+
 class Solution:
+
     def countCompleteSubarrays(self, nums: list[int]) -> int:
 
-        # Нахождение первого отрезка, в котором будут все числа из массива,
-        # а также подсчет кол-ва элементов в отрезке.
-        left, right, unique_nums = 0, 0, set(nums)
-        nums_count = {num: 0 for num in unique_nums}
-        while unique_nums:
-            unique_nums.discard(nums[right])
-            nums_count[nums[right]] += 1
-            right += 1
+        right, complete_subarrays_count, unique_count = 0, 0, len(set(nums))
+        subarray_elements = collections.defaultdict(int)
 
-        # Кол-во возможных вариантов, где начало подмассива это самый элемент
-        # равно разнице длины и right, потому что до right встретились все
-        # числа, это первый подмассив. Остальные подмассивы получаются добав-
-        # лением под одному элементу.
-        count = len(nums) - right + 1
+        # Для каждого i-го элемента left
+        for left in range(len(nums)):
 
-        # Далее нужно убирать левый элемент и если больше в подмассиве нет
-        # таких элементов, то нужно искать такой же правый. После того как в
-        # подмассиве снова есть все элементы, прибавить кол-во новых вариантов.
-        while left < len(nums):
-            num = nums[left]
-            nums_count[num] -= 1
-            left += 1
-
-            # Попытка найти элемент, если его не хватает, чтобы подмассив 
-            # удовлетворял условию.
-            while right < len(nums) and nums_count[num] == 0:
-                nums_count[nums[right]] += 1
+            # нужно найти такой минимальный элемент right, так что на отрезке
+            # left:right есть все элементы из массива.
+            while right < len(nums) and len(subarray_elements) < unique_count:
+                subarray_elements[nums[right]] += 1
                 right += 1
-
-            # Если выполнялся поиск недостающего элемента, right мог выйти за
-            # границы массива, при этом элемент мог не найтись.
-            if nums_count[num] != 0:
-                count += len(nums) - right + 1
             
-            # Если элемент не нашелся и right вышел за границу массива, то
-            # больше подмассивов удовлетворяющих условию нет.
-            elif right == len(nums):
-                return count
-        
-        return count
+            # Если кол-во уникальных элементов в подмассиве меньше, чем в
+            # массиве, значит right дошел до границы масссива, и тогда больше
+            # нет подмассивов, удовлетворяющих условию.
+            if len(subarray_elements) < unique_count:
+                break
+
+            # Иначе кол-во подмассивов для left, удовлетворяющих условию, равно
+            # кол-ву элементов len(nums) - right + 1 (right включая), так как
+            # для i-го left все подмассивы left:j, где j >= right удовлетворяют
+            # условию.
+            complete_subarrays_count += len(nums) - right + 1
+
+            # Вычетание текущего nums[left] из подсчета кол-ва элементов в
+            # подмассиве.
+            subarray_elements[nums[left]] -= 1
+            if subarray_elements[nums[left]] == 0:
+                del subarray_elements[nums[left]]
+
+        return complete_subarrays_count
