60d4047ecc5fff99d34282ee723d8d12ca637dfd
Petrov Mikhail <petrov.ma@icloud.com>
2025-04-26 02:57:42 +0300

    add task

---
+++ 45_Jump_game_ii.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/jump-game-ii/
+
+
+class Solution:
+    def jump(self, nums: list[int]) -> int:
+
+        # jumps - текущее кол-во прыжков. furthest - самая дальняя ячейка,
+        # которая могла быть достигнута на момент ДО обхода i-го элемента
+        # массива. current_furthest - самая дальнаяя ячейка на момент прыжка
+        # с i-го элемента.
+        jumps = furthest = current_furthest = 0
+
+        for i in range(len(nums) - 1):
+
+            # Если до ячейки i + x (x любое число) можно допрыгнуть минимум
+            # за k шагов, то до ячейки i можно также допрыгнуть за k шагов.
+            # Это значит, что если предыщущего current_furthest (i + x) было
+            # сделано k прыжков то до нового current_furthest можно допрыгнуть
+            # за k прыжков.
+            current_furthest = max(current_furthest, i + nums[i])
+
+            # Если i-ый элемент самый дальний, до которого можно было
+            # допрыгнуть, то чтобы допрыгнуть до current_furthest нужно
+            # сделать на один прыжок больше.
+            if i == furthest:
+                jumps += 1
+                furthest = current_furthest
+
+        return jumps
